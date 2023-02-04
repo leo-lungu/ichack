9328aa2c880b0bc8acd1617ddd8b4f94e424401d
Dylan Karrass <dylankarrass@hotmail.co.uk>
2023-02-04 17:54:06 +0000

    more gui stuff

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import font
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -6,23 +8,36 @@ class Application(tk.Frame):
         self.create_widgets()
         self.count = 0
 
+        self.defaultFont = font.nametofont("TkDefaultFont")
+        self.defaultFont.configure(family="Arial",
+                                   size=19,
+                                   weight=font.BOLD)
+        
+
     def create_widgets(self):
         self.welcome = tk.Label(self)
         self.welcome["text"] = "Welcome to Hand Hero\nCommunication made easy."
-        self.welcome.pack(side="top")
+        self.welcome.grid(row=0, column=0, columnspan=2, pady=50)
+
+
+        self.startBtn = tk.Button(self)
+        self.startBtn["text"] = "Select"
+        #self.startBtn["command"] = 
+        self.startBtn.grid(row=1, column=0)
 
         self.startBtn = tk.Button(self)
         self.startBtn["text"] = "Start"
         #self.startBtn["command"] = 
-        self.startBtn.pack(side="top")
+        self.startBtn.grid(row=1, column=1)
 
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.grid(row=2, column=0, columnspan=2, pady=50)
 
-
-root = tk.Tk()
-root.geometry("600x400+100+100")
-app = Application(master=root)
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("600x400+100+100")
+    root.title("Hand Hero")
+    app = Application(master=root)
+    app.mainloop()
