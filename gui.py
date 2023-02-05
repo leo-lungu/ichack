67eab310d09d718525cf3ec7dce6d7ea6f94aa90
Dylan Karrass <dylankarrass@hotmail.co.uk>
2023-02-05 01:56:57 +0000

    added some cooooolers

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ class Application(tk.Frame):
         self.pack()
         self.menu_page = MenuPage(self)
         self.main_page = MainPage(self)
+        self.master.configure(background='#57C4E5')
         self.menu_page.pack()
         self.defaultFont = font.nametofont("TkDefaultFont")
         self.defaultFont.configure(family="Arial",
@@ -17,29 +18,35 @@ class Application(tk.Frame):
         self.menu_page.pack_forget()
         self.main_page.pack()
 
+    def switch_to_menu(self):
+        self.main_page.pack_forget()
+        self.menu_page.pack()
+
 class MenuPage(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.configure(background='#57C4E5')
         self.pack()
         self.create_widgets()
 
+
     def create_widgets(self):
-        self.welcome = tk.Label(self)
-        self.welcome["text"] = "Welcome to Hand Hero\nCommunication made easy."
+        self.welcome = tk.Label(self, bg="#57C4E5")
+        self.welcome["text"] = "Welcome to Hand Hero!\nCommunication made easy."
         self.welcome.grid(row=0, column=0, columnspan=2, pady=50)
 
-        self.startBtn = tk.Button(self)
-        self.startBtn["text"] = "Select"
+        self.selectBtn = tk.Button(self, bg='#011627', fg='#FFFFFF')
+        self.selectBtn["text"] = "Select"
         #self.startBtn["command"] = 
-        self.startBtn.grid(row=1, column=0)
+        self.selectBtn.grid(row=1, column=0)
 
-        self.startBtn = tk.Button(self)
+        self.startBtn = tk.Button(self, bg='#011627', fg='#FFFFFF')
         self.startBtn["text"] = "Start"
         self.startBtn["command"] = self.master.switch_to_main
         self.startBtn.grid(row=1, column=1)
 
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
+        self.quit = tk.Button(self, text="QUIT", fg="red", bg='#011627', 
                               command=root.destroy)
         self.quit.grid(row=2, column=0, columnspan=2, pady=50)
 
@@ -50,13 +57,14 @@ class MainPage(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.test = tk.Label(self)
-        self.test["text"] = "This is a test."
-        self.test.grid(row=0, column=0)
+        self.back = tk.Button(self)
+        self.back["text"] = "Go back."
+        self.back["command"] = self.master.switch_to_menu
+        self.back.grid(row=0, column=0)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("600x400+100+100")
+    root.geometry("1280x720")
     root.title("Hand Hero")
     app = Application(master=root)
     app.mainloop()
